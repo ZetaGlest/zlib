6d1790c6ac6023062a948e0f53e1d7c995a9d588
mathusummut <mathusum.mut@gmail.com>
2018-03-13 22:33:56 +0100

    VS build files

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mathu/Downloads/zetaglest-source-develop/mk/windows/dependencies/src/zlib
+# Build directory: C:/Users/mathu/Downloads/zetaglest-source-develop/mk/windows/dependencies/src/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
